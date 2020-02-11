4fbab431c92ffc27594bf4c5da447c884a06c89a
Oralia <oraliagarrido@gmail.com>
2020-02-11 13:19:07 -0600

    codifica y decodifica, pero aun no muestra en pantalla

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,54 @@
-const cipher = {
-  // ...
-};
+const cipher ={code,decode};
+
+//inicia  la funcion
+function code(offset,word){
+  //inicia la funcion
+    for(let  j=0; j<word.length;j=j+1){
+      //Obtiene la posicion del primer caracter a cifrar en el codigo asc11
+      let i=word.charCodeAt(j);
+      //Desplaza el caracter offset posiciones
+      i= (i-65+offset) %26+65;
+      //variable que guarda la palabra codificada
+
+      //Guarda en una cadena el caracter desplazado (en codigo asc11)
+      let wordcode;
+      wordcode+= String.fromCodePoint(i);
+    }
+    console.log(word);
+    //imprime la palabra codificada
+    console.log(wordcode);
+
+}
+//imprime la funcion
+//console.log (code(offset,word));
+
+
+//texto a desplazar
+//let wordToDecode=document.getElementById("textdecode").value;
+
+function decode (offset,word){
+
+  //variable que guarda la palabra codificada
+  let wordDecode='';
+
+  //inicia  la funcion
+  for(let  j=0; j<word.length; j=j+1){
+    //Obtiene la posicion en codigo asc11 del caracter
+    let i=word.charCodeAt(j);
+    //Desplaza el caracter
+    i= (i+65-offset) %26+65;
+    //Guarda en una cadena el caracter desplazado (en codigo asc11)
+    wordDecode += String.fromCodePoint(i);
+  }
+
+  //imprime la palabra codificada
+console.log(wordDecode);
+}
+//imprime la funcion
+//console.log (decode(offset,word));
+
+//export default decode;
+//____________________________
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import cipher from './cipher.js';
+//variable que toma el valor a desplazar
+let offset= parseInt(document.getElementById("offset").value);
+//texto a cifrar
+let word=document.getElementById("texto").value;
 
+import cipher from './cipher.js';
 console.log(cipher);
+
+function cCode() {
+  let textCode=cipher.code(offset.value, word.value);
+  document.getElementById("textcode").innerHTML = textCode;
+};
+
+function cDecode() {
+  let textCode=cipher.decode(offset.value,word.value);
+  document.getElementById("textcode").innerHTML = textCode;
+};
