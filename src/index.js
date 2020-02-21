cdb6caf8e905a3d9970ae8f677d5ba891ce9949e
Oralia <oraliagarrido@gmail.com>
2020-02-21 10:25:43 -0600

    finaL

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import cipher from './cipher.js';
 
 let offset= document.getElementById("offset");
 let word=document.getElementById("texto");
-let pantalla3=document.getElementById("mensaje");
 
+function welcome(){
+document.getElementById("root").style.display="none";
+document.getElementById("data").style.display="block";
+}
+
+function smessage() {
+let nombre=document.getElementById("name").value;
+
+document.getElementById("wMessage").innerHTML = "Hello "+nombre+" ";
+  document.getElementById("root").style.display="none";
+  document.getElementById("data").style.display="none";
+  document.getElementById("message").style.display="block";
+}
 function cEncode() {
   let textEncode=cipher.encode( offset.value,word.value);
   document.getElementById("texto1").innerText=textEncode;
@@ -15,5 +27,7 @@ function cDecode() {
 }
 
 
+document.getElementById("go").addEventListener("click",welcome);
+document.getElementById("start").addEventListener("click",smessage);
 document.getElementById("Cifrar").addEventListener("click",cEncode);
 document.getElementById("Descifrar").addEventListener("click",cDecode);
